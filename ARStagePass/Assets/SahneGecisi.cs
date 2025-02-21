284136038e4a48d63b1b9d65f4bb6765cdf24708
Baha Yolal <bahayll221@gmail.com>
2025-02-22 01:34:19 +0300

    Completed Interactive Face Filter and AR Stage Pass Project

---
+++ ARStagePass/Assets/SahneGecisi.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SahneGecisi : MonoBehaviour
+{
+    public void sahneGecisi(string a)
+    {
+        SceneManager.LoadScene(a);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
